b0abb0d3ae832d9fab1c7d66db5ccbf39f442a4d
daibeemachine <ols00559@morris.umn.edu>
2025-08-28 14:39:26 -0500

    Some preliminary quicksort code (w/o the comparable implementation)

--- QuickSort.java
+++ QuickSort.java
@@ -1,3 +1,18 @@
 public class QuickSort {
-    
+    public static int partition(TestInteger[] array, int left, int right, int pivot)
+    {
+        while(left <= right)
+        {
+            while(array[left].compareTo(array[pivot]) < 0) { left++; }
+            while((right >= left) && (array[right].compareTo(array[pivot]) >= 0)) { right--; }
+            if(right > left)
+            {
+                TestInteger temp = array[left];
+                array[left] = array[right];
+                array[right] = temp;
+            }
+        }
+        return left;
+        //return 1;
+    }
 }
